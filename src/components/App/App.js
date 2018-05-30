44b714142437df6dca4476b3596af25ff84990da
AkyunaAkish <akyunaakish@gmail.com>
2018-05-29 22:23:32 -0600

    added initial hero image component code

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,22 @@
 import React, { PureComponent } from 'react';
 
+import HeroImage from '../HeroImage/HeroImage';
+
 class App extends PureComponent {
     render() {
         return (
             <div className='app'>
-                <h1>App</h1>
+                <HeroImage height={ 500 }
+                           width='100%'
+                           opacity={ 0.3 }
+                           overlayColor='rgb(0,0,0)'
+                           imageSrc='https://images.pexels.com/photos/640781/pexels-photo-640781.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260'
+                           overlayContent={
+                               <div>
+                                    <h1>Hello World!</h1>
+                               </div>
+                           }
+                           overlayContentCentered={ true } />
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './sass/style.scss';
+
 import App from './components/App/App';
 
 ReactDOM.render(<App />, document.querySelector('#app'));
